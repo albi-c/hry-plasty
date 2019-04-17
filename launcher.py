a97eb7918644f828f8579170832ca1ec46fac54b
albi-c <albert.csontos@gmail.com>
2019-04-17 15:18:02 +0200

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -1,2 +1,33 @@
-def launch(name):
-  pass
+import webbrowser
+from threading import Thread
+import sys
+
+class Game:
+    games = {"plasty-papir-ostatni": {"type": "web", "url": "plasty-papir-ostatni/index.html"}}
+    def __init__(self, name):
+        self.name = name
+        self.type = Game.gettype(name)
+    def start(self):
+        if self.type == "web":
+            Game.openweb(Game.getweburl(self.name))
+        else:
+            Game.openother(name)
+    def gettype(name):
+        return Game.games[name]["type"]
+    def getweburl(name):
+        return Game.games[name]["url"]
+    def openweb(url):
+        webbrowser.open(url)
+    def openother(name):
+        # work for ParicBat to implement starting of c# games
+        return None
+
+def start_(name):
+    game = Game(name)
+    game.start()
+
+def start(name):
+    th = Thread(target=start_, args=(name,))
+    th.start()
+
+start(sys.argv[1])
